c8442375a4a6d44066892aeaca8aaa567500d6f3
claudio990 <83033736+claudio990@users.noreply.github.com>
2023-04-28 13:59:19 -0600

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { NFC, Ndef } from '@awesome-cordova-plugins/nfc/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,46 @@ import { IonicModule } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [IonicModule],
+  providers:[NFC, Ndef]
 })
 export class HomePage {
-  constructor() {}
+  tag : any = "NO";
+
+  constructor(private nfc: NFC, private ndef: Ndef) {
+    this.nfc.enabled();
+  }
+
+  nfcActive(){
+    this.nfc.addTagDiscoveredListener(() => {
+      console.log('successfully attached ndef listener');
+    }, (err:any) => {
+      console.log('error attaching ndef listener', err);
+    }).subscribe((event:any) => {
+      console.log('received ndef message. the tag contains: ', event.tag);
+      console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
+      this.tag = this.nfc.bytesToHexString(event.tag.id);
+      let message = this.ndef.textRecord('Hello world');  
+      // this.nfc.share([message]).then(this.onSuccess).catch(this.onError);
+    });
+  }
+  nfcWrite(){
+    console.log('los');
+    
+    var message = [
+        this.ndef.textRecord("hello, world"),
+        this.ndef.uriRecord("http://github.com/chariotsolutions/phonegap-nfc")
+    ];
+    console.log('primero');
+    
+    this.nfc.write(message);
+
+    console.log('aaaa');
+    
+  }
+  onSuccess(){
+    this.tag = "LEÍDO";
+  }
+  onError(){
+    this.tag = "NO LEÍDO";
+  }
 }
